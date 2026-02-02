4378737ca68a68d22cf62ff680108855362dccb6
Astrallist <132389598+Astrallist@users.noreply.github.com>
2026-02-02 21:55:00 +0300

    Update invitedUsers.js

--- src/invitedUsers.js
+++ src/invitedUsers.js
@@ -1,12 +1,12 @@
 export const invitedUsers = [
 
-    { firstName: "Дарья", lastName: "Севостьянова", shortName: 'Даша', sex: "женский", hook: "Работа", status: null, friends: ['Андрей_Дуяр']},
-    { firstName: "Андрей", lastName: "Дяур", shortName: 'Андрей', sex: "мужской", hook: "Работа", status: null, friends: ['Дарья_Севостьянова'] },
+    { firstName: "Дарья", lastName: "Савостьянова", shortName: 'Даша', sex: "женский", hook: "Работа", status: null, friends: ['Андрей_Дуяр']},
+    { firstName: "Андрей", lastName: "Дяур", shortName: 'Андрей', sex: "мужской", hook: "Работа", status: null, friends: ['Дарья_Савостьянова'] },
     { firstName: "Евгений", lastName: "Гоцев", shortName: 'Женя', sex: "мужской", hook: "Школа", status: null, friends: ['Анастасия_Гоцева','Максим_Москалев'] },
     { firstName: "Анастасия", lastName: "Гоцева", shortName: 'Настя', sex: "женский", hook: "Бар", status: null, status: null, friends: ['Евгений_Гоцев'] },
     { firstName: "Максим", lastName: "Москалев", shortName: 'Максим', sex: "мужской", hook: "", status: "VIP", friends: ['Евгений_Гоцев'] },
     { firstName: "Ася", lastName: "Шарова", shortName: 'Ася', sex: "женский", hook: "", status: null, friends: ['Максим_Москалев'] },
     { firstName: "Дария", lastName: "Которова", shortName: 'Даша', sex: "женский", hook: "", status: null, friends: null },
 
-    { firstName: "Дмитрий", lastName: "Сергеев", shortName: '', sex: "мужской", hook: "", status: "VISITOR", friends: ['Андрей_Дуяр','Дарья_Севостьянова','Максим_Москалев','Евгений_Гоцев','Анастасия_Гоцева','Дария_Которова','Ася_Шарова'] }
+    { firstName: "Дмитрий", lastName: "Сергеев", shortName: '', sex: "мужской", hook: "", status: "VISITOR", friends: ['Андрей_Дуяр','Дарья_Савостьянова','Максим_Москалев','Евгений_Гоцев','Анастасия_Гоцева','Дария_Которова','Ася_Шарова'] }
 ];
